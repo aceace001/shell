5d3533d9218ab414b4677d14b97ebdab6b72bbf4
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-10-20 13:03:22 -0700

    Update sshell.c

--- sshell.c
+++ sshell.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
+
+#define CMDLINE_MAX 512
+
+int main(void)
+{
+  char cmd[CMDLINE_MAX];
+  char *args[CMDLINE_MAX] = {cmd, "hello", NULL};
+  while (1) {
+    char *nl;
+    int retval;
+    pid_t pid;
+    /* Print prompt */
+    printf("sshell@ucd$ ");
+    fflush(stdout);
+
+    /* Get command line */
+    fgets(cmd, CMDLINE_MAX, stdin);
+    /* Print command line if stdin is not provided by terminal */
+    if (!isatty(STDIN_FILENO)) {
+      printf("%s", cmd);
+      fflush(stdout);
+    }
+
+    /* Remove trailing newline from command line */
+    nl = strchr(cmd, '\n');
+    if (nl)
+      *nl = '\0';
+
+    /* Builtin command */
+    if (!strcmp(cmd, "exit")) {
+      fprintf(stderr, "Bye...\n");
+      break;
+}
+
+    /* Regular command */
+    retval = system(cmd);
+    fprintf(stderr, "Return status value for '%s': %d\n", cmd, retval);
+}
+
+return EXIT_SUCCESS;
+}
