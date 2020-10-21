7d8142b4a18f06476f5b316a3c01886b7d1ed190
zwucd166 <72957266+zwucd166@users.noreply.github.com>
2020-10-21 02:24:52 -0700

    Update temp.c

--- temp.c
+++ temp.c
@@ -7,112 +7,116 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-
 #define CMDLINE_MAX 512
+#define ARGS_MAX 16
+#define TOKEN_MAX 32
 
 struct command {
-    char *argument[16];
-    char *commands[32];
-    int count;
+  char *input;  // full command input
+  char *cmd;    // cmd = args[0]
+  char *args[ARGS_MAX];
+  int numOfArgs;
 };
 
-int parse (struct command *cmd) {
-    char buffer[CMDLINE_MAX];
-    char *temp = malloc(CMDLINE_MAX);
+struct command readParse(char* cmd){
+  struct command command;
+  char *nl;
+  char* token = malloc(TOKEN_MAX*sizeof(char));
 
-    /* Get command line */
-    fgets(buffer, CMDLINE_MAX, stdin);
+  /* Get command line */
+  fgets(cmd, CMDLINE_MAX, stdin);
 
-    /* Print command line if stdin is not provided by terminal */
-    if (!isatty(STDIN_FILENO)) {
-        printf("%s", buffer);
-        fflush(stdout);
-    }
+  /* Print command line if stdin is not provided by terminal */
+  if (!isatty(STDIN_FILENO)) {
+    printf("%s", cmd);
+    fflush(stdout);
+  }
 
-    cmd->commands[0] = strtok(strcpy(temp, buffer), "\n");
-
-    cmd->count = 0;
-    char* token = strtok(cmd->commands[0], " ");
-    cmd->argument[cmd->count] = token;
-    while (token != NULL) {
-        token = strtok(NULL, " ");
-        cmd->argument[cmd->count + 1] = token;
-        cmd->count ++;
-    }
-
-    return 0;
-
+  /* Remove trailing newline from command line */
+  nl = strchr(cmd, '\n');
+  if (nl)
+    *nl = '\0';
+  command.input = cmd;
+  token = strtok(cmd," ");
+  command.args[0] = token;
+  command.numOfArgs = 1;
+  while(token != NULL){
+    token = strtok(NULL, " ");
+    command.args[command.numOfArgs] = token;
+    command.numOfArgs++;
+  }
+  command.cmd = command.args[0];
+  return command;
 }
 
-int buildin (struct command *cmd) {
-    if (!strcmp(cmd->argument[0], "pwd")) {
-        char cwd[CMDLINE_MAX];
-        getcwd(cwd, CMDLINE_MAX);
-        printf("%s\n", cwd);
-        fprintf(stderr, "+ completed '%s' [0]\n",
-                cmd->commands[0]);
-    }
-    else if (!strcmp(cmd->argument[0], "cd")){
-        int error = chdir(cmd->argument[1]);
-        int status = 0;
-        if (error == -1){
-            fprintf(stderr, "Error: cannot cd into directory\n");
-            status = 1;
-        }
-        fprintf(stderr, "+ completed '%s' [%d]\n",
-                cmd->commands[0], status);
-
-    }
-    else if (!strcmp(cmd->argument[0], "exit")) {
-        fprintf(stderr, "Bye...\n");
-        fprintf(stderr, "+ completed '%s' [0]\n",
-                cmd->commands[0]);
-        exit(0);
-    }
-    return 0;
+void pwd(struct command command) {
+  if (!strcmp(command.input, "pwd")) {
+    char cwd[CMDLINE_MAX];
+    getcwd(cwd, CMDLINE_MAX);
+    printf("%s\n", cwd);
+    fprintf(stderr, "+ completed '%s' [0]\n", command.input);
+    exit(0);
+  }
 }
 
-int num(struct command *cmd){
-    if ((!strcmp(cmd->argument[0], "pwd")) | (!strcmp(cmd->argument[0], "cd")) |
-        (!strcmp(cmd->argument[0], "exit"))){
-        return 1;
+void cd(struct command command) {
+  if (!strcmp(command.cmd, "cd")){
+    int error = chdir(command.args[1]);
+    int status = 0;
+    if (error == -1){
+      fprintf(stderr, "Error: cannot cd into directory\n");
+      status = 1;
+    }
+    fprintf(stderr, "+ completed '%s' [%d]\n", command.input, status);
+    exit(0);
+  }
+}
+
+int main(void)
+{
+  char *cmd = malloc(CMDLINE_MAX*sizeof(char));
+  struct command command;
+  while (1) {
+    int retval = 0;
+    pid_t pid;
+    /* Print prompt */
+    printf("sshell@ucd$ ");
+    fflush(stdout);
+
+    command = readParse(cmd);
+    command.args[command.numOfArgs++] = NULL;
+
+    /* Builtin command */
+    if (!strcmp(command.input, "exit")) {
+      fprintf(stderr, "Bye...\n");
+      fprintf(stderr, "+ completed '%s' [0]\n", command.input);
+      break;
+    }
+
+    /* Regular command */
+    pid = fork();
+    if(pid == 0) {
+      // pwd
+      pwd(command);
+
+      //cd
+      cd(command);
+
+      execvp(command.cmd, command.args);
+      perror("execvp");
+      exit(1);
+    } else if (pid > 0) {
+      int status;
+      waitpid(pid, &status, 0);
     } else {
-        return 0;
+      perror("fork");
+      exit(1);
     }
+    //fprintf(stderr, "Return status value for '%s': %d\n", command.input, retval);
+  }
+
+  return EXIT_SUCCESS;
 }
 
-int main(void) {
 
-    while (1) {
-        pid_t pid;
-        printf("sshell@ucd$ ");
-        fflush(stdout);
-        struct command c;
-
-        parse(&c);
-
-        if (num(&c) == 1) {
-            buildin(&c);
-        } else {
-            pid = fork();
-            int status = 0;
-            if (pid == 0) {
-                int error = execvp(c.argument[0], c.argument);
-
-                if (error == -1){
-                    fprintf(stderr, "Error: Command not found\n");
-                }
-		        exit(1);
-            } else {
-                if (waitpid(pid, &status, 0) < 0) {
-                    break;
-                }
-                fprintf(stderr, "+ completed '%s' [%d]\n", c.commands[0],status);
-
-            }
-        }
-    }
-    // code unreachable?
-    return EXIT_SUCCESS;
-}
 
